5afb2c2830fd0989317d2f4eee8a38cbcc8f52be
Bradley Showen <BShowen@me.com>
2019-03-03 10:05:01 -0500

    refactor some methods

--- my_enumerable_methods.rb
+++ my_enumerable_methods.rb
@@ -1,36 +1,30 @@
 module Enumerable 
     def my_each
-        i = 0
-        while i < self.length
-            yield(self[i])
-            i += 1
+        for i  in self
+            yield(i)
         end
     end
 
     def my_each_with_index
-        i = 0
-        while i < self.length
-            yield(self[i],i)
-            i += 1
+        index = 0
+        for i in self
+            yield(i,index)
+            index+= 1
         end
     end
 
     def my_select
         selected = []
-        i = 0
-        while i < self.length
-            selected << self[i] if ( yield(self[i]) == true )
-            i += 1
+        for i in self
+            selected << i if ( yield(i) == true )
         end
         selected
     end
 
     def my_all?
         status = true
-        i = 0
-        while i < self.length
-            if yield(self[i]) == true 
-                i += 1
+        for i in self
+            if yield(i) == true 
                 next
             else
                 status = !status
@@ -40,15 +34,12 @@ module Enumerable
         status
     end
 
-    def my_none? #{|item | item == 3 }
+    def my_none? 
         status = true
-        i = 0
-        while i < self.length
-            if yield(self[i])
+        for i in self
+            if yield(i)
                 status = !status
                 break
-            else
-                i += 1
             end
         end 
         status
@@ -57,10 +48,8 @@ module Enumerable
     def my_count count = nil
         if count
             item_count = 0
-            i = 0
-            while i < self.length
-                item_count += 1 if self[i] == count 
-                i += 1
+            for item in self
+                item_count += 1 if item == count 
             end
         else
             return self.length
@@ -71,10 +60,8 @@ module Enumerable
     #The my_map method will accept a block. 
     def my_map 
         new_array = []
-        i = 0
-        while i < self.length
-            new_array << yield(self[i])
-            i += 1
+        for i in self
+            new_array << yield(i)
         end
         new_array
     end
@@ -82,15 +69,13 @@ module Enumerable
     #The my_proc_map method will work the same as the my_map method. However it will work with a proc instead.
     def my_proc_map &proc_to_block
         new_array = []
-        i = 0
-        while i < self.length
-            new_array << proc_to_block.call(self[i])
-            i += 1
+        for i in self
+            new_array << proc_to_block.call(i)
         end
         new_array
     end
 
-    #The proc_and_block_map will accept either a block or a proc. If both block and proc are probvided then only the proc is used. 
+    #The proc_and_block_map will accept either a block or a proc.
     def proc_and_block_map &proc
         new_array = []
         for i in self
@@ -114,7 +99,5 @@ module Enumerable
         end
         accumulator 
     end
-
-
-
+    
 end 
