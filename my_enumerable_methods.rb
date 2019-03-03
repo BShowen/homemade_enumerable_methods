8606dd03b9bb342ebe56b60faa42a6976bc2e0f0
Bradley Showen <BShowen@me.com>
2019-03-03 08:04:58 -0500

    add my_map method

--- my_enumerable_methods.rb
+++ my_enumerable_methods.rb
@@ -68,4 +68,14 @@ module Enumerable
         item_count
     end
 
+    def my_map 
+        new_array = []
+        i = 0
+        while i < self.length
+            new_array << yield(self[i])
+            i += 1
+        end
+        new_array
+    end
+
 end 
